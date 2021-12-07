238667b41d7982399b9fd9e1121f141a1c5dd050
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2021-12-07 22:54:29 +0300

    day one

---
+++ src/js/components/scrollreveal.js
@@ -0,0 +1,4 @@
+// if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+//     // some code..
+// } else {}
+window.scrollReveal = new scrollReveal();
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
-$(function() {
-    console.log(1);
-});
+// $(function() {
+//     console.log(1);
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    //= components/scrollreveal.js
+
+})
