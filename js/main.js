18b74713e8c45412eac5a97b08cfe8b50dd099d6
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2021-12-07 22:55:10 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-$(function() {
-    console.log(1);
-});
+// $(function() {
+//     console.log(1);
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        // some code..
+    } else {}
+    window.scrollReveal = new scrollReveal();
+
+})
